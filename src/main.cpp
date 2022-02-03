feb0e76f9d6db23727b597332d537b81bf8d3498
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-02-02 21:10:57 -0300

    json version

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,25 @@
 #include "ESP8266WiFi.h"
 #include "ESP8266WebServer.h"
 #include "ESP8266mDNS.h"
+#include "ArduinoJson.h"
 
 #include "secrets.h"
 
 #define BLUE_LED D1
 #define YELLOW_LED D2
-
-struct ParserResult
-{
-  uint8 led_pin;
-  int digital_option;
-};
+#define LM35 A0
+#define JSON_CONTENT_TYPE "application/json"
 
 ESP8266WebServer WEB_SERVER(80);
 void connect_to_my_wifi();
 void set_pins();
 void set_routers();
 const char *get_home_page();
-ParserResult parse_request_body(String &body);
-
-
+float read_temperature_in_celsius();
 void setup()
 {
   Serial.begin(115200);
+  delay(500);
 
   set_pins();
 
@@ -36,6 +32,7 @@ void set_pins()
 {
   pinMode(BLUE_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
+  pinMode(LM35, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   digitalWrite(LED_BUILTIN, LOW);
@@ -44,7 +41,6 @@ void set_pins()
 }
 void connect_to_my_wifi()
 {
- 
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
@@ -71,24 +67,68 @@ void set_routers()
   else
     Serial.println("Unable to start MDNS");
 
+  auto ledsRouter = "/api/leds";
+  auto temperatureRouter = "/api/temperature";
+
   WEB_SERVER.on("/", HTTP_GET, []()
                 { WEB_SERVER.send(200, "text/html", get_home_page()); });
 
-  WEB_SERVER.on("/led", HTTP_POST, []()
+  WEB_SERVER.on(temperatureRouter, HTTP_GET, []()
+                {
+                  StaticJsonDocument<16> doc;
+
+                  doc["temperature_in_celsius"] = read_temperature_in_celsius();
+
+                  String stringJson;
+                  serializeJson(doc, stringJson);
+                  WEB_SERVER.send(200, JSON_CONTENT_TYPE, stringJson);
+                });
+
+  WEB_SERVER.on(ledsRouter, HTTP_GET, []()
+                {
+                  StaticJsonDocument<192> doc;
+
+                  JsonObject doc_0 = doc.createNestedObject();
+                  doc_0["id"] = BLUE_LED;
+                  doc_0["color"] = "blue";
+                  doc_0["status"] = (bool)digitalRead(BLUE_LED);
+
+                  JsonObject doc_1 = doc.createNestedObject();
+                  doc_1["id"] = YELLOW_LED;
+                  doc_1["color"] = "yellow";
+                  doc_1["status"] = (bool)digitalRead(YELLOW_LED);
+
+                  String stringJson;
+                  serializeJson(doc, stringJson);
+
+                  WEB_SERVER.send(200, JSON_CONTENT_TYPE, stringJson);
+                });
+
+  WEB_SERVER.on(ledsRouter, HTTP_POST, []()
                 {
                   auto request_body = WEB_SERVER.arg("plain");
 
                   if (request_body.isEmpty())
                   {
-                    WEB_SERVER.send(200, "text/plain", "You must send somthing!");
+                    WEB_SERVER.send(400, "text/plain", "You must send somthing in JSON Format");
                     return;
                   }
 
+                  StaticJsonDocument<96> doc;
+                  DeserializationError error = deserializeJson(doc, request_body);
+                  if (error)
+                  {
+                    Serial.print(F("deserializeJson() failed: "));
+                    Serial.println(error.f_str());
+                    WEB_SERVER.send(500, "text/plain", F("deserializeJson() failed: "));
+                    return;
+                  }
+                  int id = doc["id"];
+                  bool status = doc["status"];
+
+                  digitalWrite(id, status);
+
                   WEB_SERVER.send(201, "text/plain", request_body);
-
-                  auto result = parse_request_body(request_body);
-
-                  digitalWrite(result.led_pin, result.digital_option);
                 });
 
   WEB_SERVER.begin();
@@ -103,26 +143,33 @@ void loop()
   MDNS.update();
 }
 
-ParserResult parse_request_body(String &body)
+float read_temperature_in_celsius()
 {
-  auto delimiter = body.indexOf("&");
-  auto action_token = body.substring(delimiter + 1);
-  auto led_token = body.substring(0, delimiter);
 
-  delimiter = action_token.indexOf("=");
-  auto action_value = action_token.substring(delimiter + 1);
+  int raw_input = analogRead(LM35);
 
-  auto digital_option = action_value == "ON" ? HIGH : LOW;
+  float max_voltage_in_voltz = 3.0;
+  float max_raw_input_value = 1024;
+  float voltz_per_celsius = 0.01;
+  /*
+     Segundo o datasheet do LM35 possui 0.01V/°C
+     ou seja para para 10mV - 1°C
 
-  delimiter = led_token.indexOf("=");
-  auto led_color = led_token.substring(delimiter + 1);
+     sabendo que  3.0V - 1024
+     então:        yV  -  x
+     
+     y = 3.0V*x/1023
 
-  auto led_pin = led_color == "blue" ? BLUE_LED : YELLOW_LED;
+     sabendo que  0.01V - 1°C
+                   yV   - t°C
+  
+     t = y/0.01 
 
-  Serial.println("Body: " + body);
-  Serial.println("led: " + led_color + " action: " + action_value);
+     t = (3.0V*x/1023)/0.01
+ 
+     */
 
-  return ParserResult{led_pin, digital_option};
+  return (max_voltage_in_voltz * ((float)raw_input) / max_raw_input_value) / voltz_per_celsius;
 }
 
 const char *get_home_page()
@@ -146,34 +193,12 @@ const char *get_home_page()
 
     <div style="width: 100%; height: 100%; margin-left: 1%;">
         <h1>ESP 8266</h1>
-        <p>change led status</p>
-        <div style="display:flex; width: 100%; height: 100%;">
+        
+    <ul>
+        <li>/api/leds</li>
+        <li>/api/temperature</li>
+    </ul>
 
-            <form id="led_form" action="/led" method="post" target="dummyframe" style="width: 100%;">
-                <div class="form-floating" style=" width: 7%;margin-bottom: 1%;">
-
-                    <select class="form-select" aria-label="Floating label select example" id="led" name="led">
-                        <option value="Yellow">Yellow</option>
-                        <option value="blue">Blue</option>
-                    </select>
-
-                    <label for="led">Choose a led:</label>
-                </div>
-
-
-
-                <input class="btn btn-success" style="margin-right: 1.2%;" id="submit" type="submit" name="action"
-                    value="ON">
-                <input class="btn btn-danger" id="submit" type="submit" name="action" value="OFF">
-            </form>
-        </div>
-    </div>
-
-
-    <iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
